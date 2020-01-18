3217b18deed0d85a6c9173234c2203b35efdc551
Preston Wallace <wallace.preston@gmail.com>
2020-01-18 11:53:05 -0800

    complete

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Even and Odd
+## An Even and Odd Number generator, using React Hooks
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const Form = ({generateNum, evens, setEvens}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='big-list-item'>
+      Even Number Only <input type='checkbox' checked={evens} onChange={() => setEvens(!evens)}></input>
+      <div>
+        <button onClick={generateNum}>Add Number</button>
+      </div>
     </div>
+  )
+}
+
+const Numbers = ({numbers}) => (
+  <div className='big-list-item'>
+    Numbers:
+  
+    <div className='list-container'>
+      {numbers.map((number, idx) => (
+        <div className='list-item' key={idx}>
+          {number}
+        </div>
+      ))}
+    </div>
+  </div>
+)
+
+const Stats = ({numbers, seen}) => (
+  <div className='big-list-item'>
+    {numbers.reduce((accumulator, num) => {
+      console.log(num)
+      console.log(seen)
+      if(!seen[num]) {
+        seen[num] = true;
+        accumulator++;
+      }
+      return accumulator;
+    }, 0)} unique numbers
+  </div>
+)
+
+const App = () => {
+  const [numbers, setNunmbers] = useState([7,4])
+  const [evens, setEvens] = useState(false)
+  const seen = {};
+  const generateNum = () => {
+    const newNum = evens 
+      ? Math.ceil(Math.random() * 10 / 2) * 2
+      : Math.ceil(Math.random() * 10)
+    setNunmbers([
+      ...numbers,
+      newNum
+    ])
+  }
+  return (
+    <main className="App ">
+      <h1>Even/Odd</h1>
+      
+      <Stats numbers={numbers} seen={seen}/>
+      <Form generateNum={generateNum} evens={evens} setEvens={setEvens}/>
+      <Numbers numbers={numbers}/>
+    </main>
   );
 }
 
